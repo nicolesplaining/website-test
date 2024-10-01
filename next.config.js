68b7f4260acfaff0186928713e519e91c6a8fd0d
Nicole <ma06nicole@gmail.com>
2024-09-30 19:16:17 -0700

    eslint fix

--- next.config.js
+++ next.config.js
@@ -8,4 +8,5 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
+  }
+}
